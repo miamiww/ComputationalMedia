59ada8c2368a6ed36bef3a478340d799aef911df
miamiworldwide <rivendalejones@gmail.com>
2017-10-03 11:23:04 -0400

    made some tweaks to animated drawing

--- Week4/animatedDrawing.js
+++ Week4/animatedDrawing.js
@@ -1,15 +1,9 @@
 
-
-
-var browserSize = {
-  x: window.innerWidth || document.body.clientWidth,
-  y: window.innerHeight || document.body.clientHeight
-}
-
-function setup(){
-  createCanvas(1000, 300);
-//  frameRate(10);
-}
+var brightBlue = [73,174,227];
+var brightGold = [225,204,39];
+var offPink = [233,71,126];
+var brightTan = [248,208,162];
+var brightGrey = [145,153,238];
 
 var you = {
   x: 50,
@@ -19,58 +13,70 @@ var you = {
   yfarB: 250,
   spacing: 50,
   length:75
+}
+
+
+function setup(){
+  createCanvas(1000, 600);
+  noStroke();
+//  frameRate(80);
+}
+
+function youSuck(i){
+  ellipse(you.x,you.ytop+i,20,20);
+  ellipse(you.x+i,you.ybottom,20,20);
+  ellipse(you.x+you.length,you.ytop+2*i,20,20);
+  ellipse(you.x+you.length-i,you.yfarB,20,20);
+
+  //O
+  ellipse(you.x+you.length+you.spacing,you.ytop+i,20,20);
+  ellipse(you.x+you.length+you.spacing+i,you.ytop,20,20);
+  ellipse(you.x+you.length+you.spacing+i,you.ybottom,20,20);
+  ellipse(you.x+2*you.length+you.spacing,you.ytop+i,20,20);
+
+  //u
+  ellipse(you.x+2*you.length+2*you.spacing,you.ytop+i,20,20);
+  ellipse(you.x+2*you.length+2*you.spacing+i,you.ybottom,20,20);
+  ellipse(you.x+3*you.length+2*you.spacing,you.ytop+i,20,20);
+
+  //s
+  ellipse(you.x+4*you.length+4*you.spacing-i,you.ytop,20,20);
+  ellipse(you.x+3*you.length+4*you.spacing,you.ytop+i/2,20,20);
+  ellipse(you.x+3*you.length+4*you.spacing+i,you.ytop+you.length/2,20,20);
+  ellipse(you.x+4*you.length+4*you.spacing,you.ytop+you.length/2+i/2,20,20);
+  ellipse(you.x+4*you.length+4*you.spacing-i,you.ytop+you.length,20,20);
+
+  //u
+  ellipse(you.x+4*you.length+5*you.spacing,you.ytop+i,20,20);
+  ellipse(you.x+4*you.length+5*you.spacing+i,you.ybottom,20,20);
+  ellipse(you.x+5*you.length+5*you.spacing,you.ytop+i,20,20);
+
+  //c
+  ellipse(you.x+5*you.length+6*you.spacing,you.ytop+i,20,20);
+  ellipse(you.x+5*you.length+6*you.spacing+i,you.ybottom,20,20);
+  ellipse(you.x+5*you.length+6*you.spacing+i,you.ytop,20,20);
+
+  //k
+  ellipse(you.x+6*you.length+7*you.spacing,you.ytopT+1.66*i,20,20);
+  ellipse(you.x+6*you.length+7*you.spacing+i,you.ybottom-75/2+i/2,20,20);
+  ellipse(you.x+6*you.length+7*you.spacing+i,you.ybottom-75/2-i/2,20,20);
 
 }
 
 function draw(){
-  fill(0);
-  var i = frameCount;
-//  for(var i = 0; i <= 75;i++){
-    //Y
-    ellipse(you.x,you.ytop+i,20,20);
-    ellipse(you.x+i,you.ybottom,20,20);
-    ellipse(you.x+you.length,you.ytop+2*i,20,20);
-    ellipse(you.x+you.length-i,you.yfarB,20,20);
 
-    //O
-    ellipse(you.x+you.length+you.spacing,you.ytop+i,20,20);
-    ellipse(you.x+you.length+you.spacing+i,you.ytop,20,20);
-    ellipse(you.x+you.length+you.spacing+i,you.ybottom,20,20);
-    ellipse(you.x+2*you.length+you.spacing,you.ytop+i,20,20);
+  var time = frameCount % 600;
 
-    //u
-    ellipse(you.x+2*you.length+2*you.spacing,you.ytop+i,20,20);
-    ellipse(you.x+2*you.length+2*you.spacing+i,you.ybottom,20,20);
-    ellipse(you.x+3*you.length+2*you.spacing,you.ytop+i,20,20);
-
-    //s
-    ellipse(you.x+4*you.length+4*you.spacing-i,you.ytop,20,20);
-    ellipse(you.x+3*you.length+4*you.spacing,you.ytop+i/2,20,20);
-    ellipse(you.x+3*you.length+4*you.spacing+i,you.ytop+you.length/2,20,20);
-    ellipse(you.x+4*you.length+4*you.spacing,you.ytop+you.length/2+i/2,20,20);
-    ellipse(you.x+4*you.length+4*you.spacing-i,you.ytop+you.length,20,20);
-
-    //u
-    ellipse(you.x+4*you.length+5*you.spacing,you.ytop+i,20,20);
-    ellipse(you.x+4*you.length+5*you.spacing+i,you.ybottom,20,20);
-    ellipse(you.x+5*you.length+5*you.spacing,you.ytop+i,20,20);
-
-    //c
-    ellipse(you.x+5*you.length+6*you.spacing,you.ytop+i,20,20);
-    ellipse(you.x+5*you.length+6*you.spacing+i,you.ybottom,20,20);
-    ellipse(you.x+5*you.length+6*you.spacing+i,you.ytop,20,20);
-
-    //k
-    ellipse(you.x+6*you.length+7*you.spacing,you.ytopT+1.66*i,20,20);
-    ellipse(you.x+6*you.length+7*you.spacing+i,you.ybottom-75/2+i/2,20,20);
-    ellipse(you.x+6*you.length+7*you.spacing+i,you.ybottom-75/2-i/2,20,20);
-
-//  }
-//  ellipse(mouseX,mouseY,20,20);
   if(mouseIsPressed){
-    i = 0;
-    background(255);
-  }
+    fill(255);
+    youSuck(time);
+//    for(let ii = 0; ii<=75;ii++){
+//        youSuck(ii);
+//    }
 
+  } else{
+    fill(0);
+    youSuck(time);
+  }
 
 }
