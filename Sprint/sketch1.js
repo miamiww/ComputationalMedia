2207d691d0f9373d1cfdef4b65e7ed665e928d32
miamiworldwide <rivendalejones@gmail.com>
2017-10-27 12:38:01 -0400

    mid sprint commit

--- Sprint/sketch1.js
+++ Sprint/sketch1.js
@@ -1,12 +1,39 @@
 
-var api = "http://chroniclingamerica.loc.gov/about/api/"
+var api = "http://chroniclingamerica.loc.gov/search/pages/results/?andtext=";
+var searchTerm = "Seattle";
+var format = "&format=json";
+var url = api + searchTerm + format;
+var button;
+var inputValue;
 
+function preload(){
+  data = loadJSON(url);
+}
 
 function setup(){
-  field = createInput("Search");
+  createCanvas(400,400);
+  field = createInput(searchTerm);
+  field.input(inputEvent);
   field.position(10,height-50);
+  button = createButton("Search");
+  button = select('#submit');
+  button.mousePressed(getLOCdata);
+  searchTerm = select('#term');
+
+
 }
 
 function draw(){
+  inputValue = number(data["items"][0].date);
 
+}
+
+function inputEvent(){
+
+}
+
+function getLOCData(){
+  searchTerm = newSearch;
+  url = api + searchTerm + format;
+  data = loadJSON(url);
 }
