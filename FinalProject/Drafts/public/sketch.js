838740d663c2f6cdaeed3f6980e7760194da77e6
miamiworldwide <rivendalejones@gmail.com>
2017-11-24 22:01:03 -0500

    adding game text

--- FinalProject/Drafts/public/sketch.js
+++ FinalProject/Drafts/public/sketch.js
@@ -8,6 +8,35 @@ var leftButtonRecord = [];
 var rightButtonRecord = [];
 var lastButtonClicked;
 
+//set all text displayed
+var beasts = ["ghost", "elf", "unicorn", "chimera", "goblin"];
+var lands = ["forest", "marsh", "velt", "crag", "tundra"];
+var gameText = ["it's your first day on the job as Cambridge Analytica's new psychographic neural network! can you correctly categorize these humans' personality types based on their responses to a Buzzfeed matching quiz?",
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch(),
+                beast2landMatch()]
+function beast2landMatch(){
+  var bucket = [0,1,2,3,4];
+
+  function  getRandomFromBucket(){
+     var randomIndex = Math.floor(Math.random()*bucket.length);
+     return bucket.splice(randomIndex, 1)[0];
+  }
+
+  return beasts[0]+" - "+lands[getRandomFromBucket()]+"\n"+
+  beasts[1]+" - "+lands[getRandomFromBucket() ]+"\n"+
+  beasts[2]+" - "+lands[getRandomFromBucket()]+"\n"+
+  beasts[3]+" - "+lands[getRandomFromBucket()]+"\n"+
+  beasts[4]+" - "+lands[getRandomFromBucket()]+"\n";
+}
+
+
+
 //setting global DOM position variables
 var canvasSize = {
   x: 400,
@@ -37,6 +66,7 @@ function setup(){
 
 function draw(){
   background(220);
+  level1QuizDisplay(totalClicks.length);
 }
 
 function beginningButton(){
@@ -62,11 +92,6 @@ function decisionButtons(){
 function buttonDeciderLeft(){
   leftButtonRecord.push(1);
   buttonRecorder("introvert");
-  if(leftButtonRecord.length==1){
-    console.log("success");
-  } else{
-    console.log("fail");
-  }
   _level2trigger();
 }
 
@@ -74,22 +99,32 @@ function buttonDeciderLeft(){
 function buttonDeciderRight(){
   rightButtonRecord.push(1);
   buttonRecorder("extrovert");
-  if(rightButtonRecord.length==1){
-    console.log("success");
-  } else{
-    console.log("fail");
-  }
   _level2trigger();
 }
 
+
+//this displays the quiz results for level 1 and puts them in draw
+function level1QuizDisplay(n){
+  if(n==0){
+    text(gameText[n],0,10,width,150);
+  } else{
+    push();
+    textAlign(CENTER,LEFT);
+    text(gameText[n],centerPoint.x,centerPoint.y)
+    pop();
+  }
+}
+
+
+
 function _level2trigger(){
   if(totalClicks.length == 10){
-    removeLevel1();
+    _removeLevel1();
     startLevel2();
   }
 }
 
-function removeLevel1(){
+function _removeLevel1(){
   rightButton.remove();
   leftButton.remove();
 }
